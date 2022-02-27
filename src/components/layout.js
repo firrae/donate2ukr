787c607f54d70925a9092affd1551302fac52136
Steven Lambe <steve@firrae.ca>
2022-02-27 18:27:25 +0000

    SEO updates

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 import { Link, graphql, useStaticQuery } from 'gatsby'
+import { Helmet } from "react-helmet";
 
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
@@ -31,6 +32,13 @@ export default function Layout({ children, location }) {
 
     return (
         <Container fluid>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Donate 2 Ukraine{`${location.pathname.replace('/', '') !== '' ? ' from ' + location.pathname.replace('/', '') : ' from Anywhere'}`}</title>
+                <link rel="canonical" href={`https://donate2ukr.com${location.pathname}`} />
+                <meta name="description" content="An open source list of organization and methods to donate to in support of Ukraine and her people." />
+                <meta name="keywords" content={`Ukraine, Donate, Humanitarian, IStandWithUkraine, ${location.pathname.replace('/', '') !== '' ? location.pathname.replace('/', '') : 'Everywhere'}, How to help from ${location.pathname.replace('/', '') !== '' ? location.pathname.replace('/', '') : 'Everywhere'}`} />
+            </Helmet>
             <Row>
                 <h1 style={h1Style}>Verified Groups Helping Ukraine</h1>
             </Row>
